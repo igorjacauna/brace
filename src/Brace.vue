275509d62189bf129012782861acba5e767f50b8
Ígor Jacaúna <igor.jacauna@gmail.com>
2018-01-29 09:39:24 -0400

    Create reference to parent access editor object

--- src/Brace.vue
+++ src/Brace.vue
@@ -3,51 +3,51 @@
 </template>
 
 <script>
-import * as brace from 'brace'
-import 'brace/ext/modelist'
-import 'brace/ext/themelist'
+import * as brace from "brace";
+import "brace/ext/modelist";
+import "brace/ext/themelist";
 
-const modelist = brace.acequire('ace/ext/modelist')
-const themelist = brace.acequire('ace/ext/themelist')
-var editor
+const modelist = brace.acequire("ace/ext/modelist");
+const themelist = brace.acequire("ace/ext/themelist");
 
 const regMap = {
-  isInt: new RegExp('^\\d+$')
-}
+  isInt: new RegExp("^\\d+$")
+};
 
 export default {
   props: {
     mode: {
       type: String,
-      default: 'json',
-      validator: (val) => modelist.modes.findIndex((mode) => mode.name === val) > -1
+      default: "json",
+      validator: val => modelist.modes.findIndex(mode => mode.name === val) > -1
     },
     theme: {
       type: String,
-      default: 'github',
-      validator: (val) => themelist.themes.findIndex((theme) => theme.name === val) > -1
+      default: "github",
+      validator: val =>
+        themelist.themes.findIndex(theme => theme.name === val) > -1
     },
     // todo editor split
     fontsize: {
       type: String,
-      default: '12px',
-      validator: (val) => parseInt(val) > 11 && parseInt(val) < 25
+      default: "12px",
+      validator: val => parseInt(val) > 11 && parseInt(val) < 25
     },
     codefolding: {
       type: String,
-      default: 'markbegin',
-      validator: (val) => ['manual', 'markbegin', 'markbeginend'].includes(val)
+      default: "markbegin",
+      validator: val => ["manual", "markbegin", "markbeginend"].includes(val)
     },
     // todo key binding
     softwrap: {
       type: String,
-      default: 'free',
-      validator: (val) => (['off', 'free'].includes(val) || regMap.isInt.test(val))
+      default: "free",
+      validator: val => ["off", "free"].includes(val) || regMap.isInt.test(val)
     },
     selectionstyle: {
       type: String,
-      default: 'text',
-      validator: (val) => ['text', 'line'].includes(val)
+      default: "text",
+      validator: val => ["text", "line"].includes(val)
     },
     highlightline: {
       type: Boolean,
@@ -57,58 +57,67 @@ export default {
   },
 
   methods: {
-    setMode () {
-      let modeObj = modelist.modesByName[this.mode]
+    setMode() {
+      let modeObj = modelist.modesByName[this.mode];
 
       if (modeObj) {
-        require('brace/mode/' + modeObj.name)
-        editor.getSession().setMode(modeObj.mode)
+        require("brace/mode/" + modeObj.name);
+        this.editor.getSession().setMode(modeObj.mode);
       }
     },
-    setTheme () {
-      let themeObj = themelist.themesByName[this.theme]
+    setTheme() {
+      let themeObj = themelist.themesByName[this.theme];
 
       if (themeObj) {
-        require('brace/theme/' + themeObj.name)
-        editor.setTheme(themeObj.theme)
+        require("brace/theme/" + themeObj.name);
+        this.editor.setTheme(themeObj.theme);
       }
     },
-    emitCode () {
-      this.$emit('code-change', editor.getValue())
+    emitCode() {
+      this.$emit("code-change", this.editor.getValue());
+    },
+    getContent() {
+      return this.editor.getValue();
     }
   },
 
-  mounted () {
-    editor = brace.edit('vue-bulma-editor')
-    this.setMode()
-    this.setTheme()
-    editor.$blockScrolling = Infinity
-    editor.getSession().on('change', this.emitCode)
+  data() {
+    return {
+      editor: {}
+    };
+  },
+
+  mounted() {
+    this.editor = brace.edit("vue-bulma-editor");
+    this.setMode();
+    this.setTheme();
+    this.editor.$blockScrolling = Infinity;
+    this.editor.getSession().on("change", this.emitCode);
   },
 
   watch: {
-    mode () {
-      this.setMode()
+    mode() {
+      this.setMode();
     },
-    theme () {
-      this.setTheme()
+    theme() {
+      this.setTheme();
     },
-    fontsize (newVal) {
-      editor.setFontSize(newVal)
+    fontsize(newVal) {
+      this.editor.setFontSize(newVal);
     },
-    codefolding (newVal) {
-      editor.session.setFoldStyle(newVal)
-      editor.setShowFoldWidgets(newVal !== 'manual')
+    codefolding(newVal) {
+      this.editor.session.setFoldStyle(newVal);
+      this.editor.setShowFoldWidgets(newVal !== "manual");
     },
-    softwrap (newVal) {
-      editor.setOption('wrap', newVal)
+    softwrap(newVal) {
+      this.editor.setOption("wrap", newVal);
     },
-    selectionstyle (newVal) {
-      editor.setOption('selectionStyle', newVal)
+    selectionstyle(newVal) {
+      this.editor.setOption("selectionStyle", newVal);
     },
-    highlightline (newVal) {
-      editor.setHighlightActiveLine(newVal)
+    highlightline(newVal) {
+      this.editor.setHighlightActiveLine(newVal);
     }
   }
-}
+};
 </script>
